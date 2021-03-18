8793a31f4309d0eff4917ca80a33436bf395ecf3
Ruben-hash <58661892+Ruben-hash@users.noreply.github.com>
2021-03-18 14:54:04 +0100

    Delete php/php directory

--- php/php/serveur.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-// Création d'une requête HTTP
-var req = new XMLHttpRequest();
-// Requête HTTP GET synchrone vers le fichier message.txt publié localement
-req.open("GET", "http://127.0.0.1/edsa-bonjour/bonsoir/message.txt", false);
-// Envoi de la requête
-req.send(null);
-// Affiche la réponse reçue pour la requête
-console.log(req.responseText);
